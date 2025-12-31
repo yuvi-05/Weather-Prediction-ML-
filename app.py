ddcd2246fa914f2d164505b353f06b23bc5e86d1
Yuvraj <yuvrajdake9105@gmail.com>
2026-01-01 04:43:30 +0530

    API for current and past weather as input

--- app.py
+++ app.py
@@ -6,11 +6,53 @@ import pandas as pd
 import joblib
 import os
 import time
+import requests
 
 t1 = time.time()
 
+# Importing Weather Data from Open-Meteo API
+latitude = 18.5204    # Pune coordinates
+longitude = 73.8567
+
+url = "https://api.open-meteo.com/v1/forecast"                 # Open-Meteo API for historical and current weather data
+
+params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "daily": [
+        "temperature_2m_mean",
+        "precipitation_sum",
+        "surface_pressure_mean",
+        "wind_speed_10m_mean"
+    ],
+    "past_days": 1,        # yesterday
+    "forecast_days": 1,    # today
+    "timezone": "Asia/Kolkata"
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+# Create DataFrame
+df = pd.DataFrame({
+    "date": data["daily"]["time"],
+    "avg_temperature": data["daily"]["temperature_2m_mean"],
+    "total_precipitation": data["daily"]["precipitation_sum"],
+    "avg_pressure": data["daily"]["surface_pressure_mean"],
+    "avg_wind_speed": data["daily"]["wind_speed_10m_mean"]
+})
+
+print(df)
+month = df["date"][1][5:7]             # current month
+tprev = df["avg_temperature"][0]       # previous day's average temperature
+tavg = df["avg_temperature"][1]        # current day's average temperature
+prcp = df["total_precipitation"][1]    # current day's precipitation
+wspd = df["avg_wind_speed"][1]         # current day's wind speed
+pres = df["avg_pressure"][1]           # current day's pressure
+pdiff = pres - df["avg_pressure"][0]   # difference in pressure from previous day
+
 # Data Preprocessing :
-data = pd.read_csv('dataset/data.csv')
+data = pd.read_csv('dataset/data.csv')  # meteostat dataset
 data = data.drop('pdiff',axis = 1)
 data.dropna(inplace=True)
  
@@ -42,21 +84,21 @@ else:
 
 # Input Data for Prediction :
 weather = pd.DataFrame({
-        'month': [3],              # current month
-        'tprev': [27.5],           # previous day's average temperature
-        'tavg': [28.2],            # current day's average temperature
-        'prcp': [0.0],             # current day's precipitation
-        'wspd': [6.8],             # current day's wind speed
-        'pres': [1008.5],          # current day's pressure
-        'pdiff': [-0.9]            # difference in pressure from previous day
+        'month': [month],              # current month
+        'tprev': [tprev],              # previous day's average temperature
+        'tavg': [tavg],                # current day's average temperature
+        'prcp': [prcp],                # current day's precipitation
+        'wspd': [wspd],                # current day's wind speed
+        'pres': [pres],                # current day's pressure
+        'pdiff': [pdiff]               # difference in pressure from previous day
     })
 
 # Scaling the input features :
 weather = scaler.transform(weather)
 
 # Making Predictions :
-predictions = model.predict(weather) #29.9
-print(predictions.astype(float))
+predictions = model.predict(weather) #
+print("Tomorrow's Temperature in Pune :",predictions.astype(float))
 
 t2 = time.time()
 print(f"Execution Time: {t2 - t1} seconds")
